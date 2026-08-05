4e4eb97645e275f8d5f20a9ea4c901739793a7c5
Yogendar <yogendersinghrt48@gmail.com>
2026-08-05 23:39:26 +0530

    we learn about conversion in deep

---
+++ Datatypes.js
@@ -0,0 +1,62 @@
+
+// let name="hello"//datatype is string
+// let num=45 // datatype is number
+// let isOnline=true// datatype is boolean
+// let iamNull=null //datatype is object
+
+// console.log(typeof(iamNull))//wiii be object)
+
+// let iamUndefined; //data type is undefined
+// console.log(iamUndefined)//will be datatype undefined
+
+
+//***************************now what happend when we do type conversion what actually they hold ***************
+
+//->Number conversion
+// let score=90
+
+// console.log(typeof(score))
+// console.log(typeof(score))
+
+
+// let valueInNumber= Number(score)
+
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+//->outputs can be like
+//"33"=>33
+//"56abc"=>Nan um type will Nan
+//true=>1 false=>0
+
+
+//***********Boolean Conversion
+// let isLoggedIn=undefined
+
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+//output can be  like
+
+//1=>true
+//0=>false
+//''=>false
+//'yogi'=true
+//undefined=false
+//null=>false
+
+//********************* * String Conversion/
+
+let someNum=45
+
+let stringNumber= String(someNum)
+
+console.log(stringNumber)
+console.log(typeof(stringNumber))
+
+
+
+
+
+
