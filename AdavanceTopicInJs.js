13ac5450b85eb10d2e4d92575235b9fc11513dd4
Yogendar <yogendersinghrt48@gmail.com>
2026-08-08 12:47:20 +0530

    advance  concepts of js

---
+++ AdavanceTopicInJs.js
@@ -0,0 +1,12 @@
+//there is summaray of advance js concetps which we will revise 
+// [1] Closures in json
+//[2]Currying
+//[3]Destructuring
+//[4]This behvior
+//[5]Call,bind,apply (we will pratice with example)
+//[6]Debouncing
+//[7]Throttling
+//[8]ShallowCopy
+//[9]DeepCopy
+//[10]Prototypes
+//[11]ops Concepts in JavaScript
