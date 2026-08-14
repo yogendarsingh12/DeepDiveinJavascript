661aa0816102123a95bc5c80aff2f0c2b85419ed
Yogendar <yogendersinghrt48@gmail.com>
2026-08-14 11:16:15 +0530

    Revised Closure Concept

--- AdavanceTopicInJs.js
+++ AdavanceTopicInJs.js
@@ -1,5 +1,5 @@
 //there is summaray of advance js concetps which we will revise 
-// [1] Closures in json
+// [1] Closures in Javascript
 //[2]Currying
 //[3]Destructuring
 //[4]This behvior
@@ -16,3 +16,26 @@
 //[15]high-orderFunctions
 //[16]callback Hell
 
+
+
+//[1]*********************************Closures******************************************
+
+//->a closure is created when an inner function remembers the lexical scope and variables of the outer function even though the outer function has returned, 
+// ->and some real-life examples are timers, React components, higher-order functions, Promises, etc
+
+// function Outer() {
+//   const name = "Yogendar Singh Rajawat";
+
+//   // Lowercase 'function'
+//   return function Inner() {
+//     console.log("Name is accessible in function:", name);
+//   };
+// }
+
+// // Uppercase 'O' to match the function definition
+// let result = Outer(); 
+
+// result(); // Logs: "Name is accessible in function: Yogendar Singh Rajawat"
+
+
+//[2]++++++++++++++++++++++++++++++++++++++Currying
